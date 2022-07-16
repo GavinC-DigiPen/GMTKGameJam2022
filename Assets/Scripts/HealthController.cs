f1e620337518fa3dfa808ffc86416dca51a31d70
joloujo <joloujo28@gmail.com>
2022-07-15 22:08:15 -0700

    Health script

---
+++ Assets/Scripts/HealthController.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthController : MonoBehaviour
+{
+
+    void OnCollisionEnter2D(Collision2D col) 
+    {
+        if (col.gameObject.tag == "Damaging") 
+        {
+            GameManger.currentHealth -= 1;
+
+            Enemy1Controller[] enemies = FindObjectsOfType<Enemy1Controller>();
+
+            foreach (Enemy1Controller enemy in enemies)
+            {
+                if ((enemy.transform.position - gameObject.transform.position).magnitude <= 5)
+                {
+                    enemy.state = "push back";
+                    enemy.state_time = 0;
+                    Debug.Log(enemy.name);
+                }
+            }
+        }
+    }
+}
